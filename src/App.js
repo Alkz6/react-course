48894ad7a7e623b15dfddbfe541bf778c18823fd
Jaime Alexis Martinez Medina <jalexismtz.medina@gmail.com>
2019-04-04 21:43:08 -0600

    primer commit

---
+++ src/App.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+import './App.css';
+import Radium, { StyleRoot } from 'radium';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: 'ussad', name: 'Alexis', age: 27 },
+      { id: 'us21312', name: 'Kitzia', age: 28 , hobbies: 'Learning languages'},
+      { id: 'ussad1', name: 'Juan', age: 26 }
+    ],
+    otherState: 'some other value'
+  };
+
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: newName, age: 27 },
+        { name: newName, age: 28 , hobbies: 'Learning languages'},
+        { name: newName, age: 26 }
+      ],
+      showPersons: false
+    });
+  };
+
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({
+      persons: persons
+    })
+    // const person = Object.assing({}, this.state.persons[personIndex]);
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+  }
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    });
+  }
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  }
+
+  render() {
+
+    const sytleButton = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px sold blue',
+      padding: '8px',
+      borderRadius: '8px',
+      cursor: 'pointer',
+      ':hover':{
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
+    }
+
+    let persons = null;
+
+    if (this.state.showPersons){
+      persons = (
+        <StyleRoot>
+          <div>
+            {this.state.persons.map((person, index) => {
+              return <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangeHandler(event, person.id)}
+                />
+            })}
+          </div>
+        </StyleRoot>
+      );
+
+      sytleButton.backgroundColor = 'red';
+      sytleButton[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2){
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1){
+      classes.push('bold');
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
+          <button 
+            style={sytleButton}
+            onClick={this.togglePersonHandler}
+          >Toggle Persons</button>
+         {persons}
+        </header>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
+
+export default Radium(App);
